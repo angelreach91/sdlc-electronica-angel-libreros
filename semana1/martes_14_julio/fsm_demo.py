78bbb86b5289587108d34769e04267826da9ca74
Angel Jesus Libreros Ponce <angelibpo@outlook.com>
2026-07-15 19:37:51 -0600

    feat: se completa la logica de la maquina de estados

--- semana1/martes_14_julio/fsm_demo.py
+++ semana1/martes_14_julio/fsm_demo.py
@@ -11,3 +11,26 @@ class TrafficLightFSM:
     def __init__(self) -> None:
         self._state = TrafficLightState.RED
         self._cycle_count = 0
+
+    @property
+    def state(self) -> TrafficLightState:
+        """Devuelve el estado actual del semáforo."""
+        return self._state
+
+    @property
+    def cycle_count(self) -> int:
+        """Devuelve el número de transiciones realizadas."""
+        return self._cycle_count
+
+    def transition(self) -> TrafficLightState:
+        """Realiza una transición y devuelve el nuevo estado."""
+        transitions = {
+            TrafficLightState.RED: TrafficLightState.GREEN,
+            TrafficLightState.GREEN: TrafficLightState.YELLOW,
+            TrafficLightState.YELLOW: TrafficLightState.RED,
+        }
+
+        self._state = transitions[self._state]
+        self._cycle_count += 1
+
+        return self._state
